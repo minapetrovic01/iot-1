1fe2e486228c3ecab3475ad69bec6b992fc20dcf
Mina <minap2001@gmail.com>
2024-04-18 01:39:42 +0200

    added mongoose module

--- grpc-service/src/app.module.ts
+++ grpc-service/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PillowModule } from './pillow/pillow.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [PillowModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/pillow'),PillowModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- grpc-service/src/pillow/pillow.module.ts
+++ grpc-service/src/pillow/pillow.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PillowService } from './pillow.service';
 import { PillowController } from './pillow.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Pillow, PillowSchema } from './pillow.model';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Pillow.name, schema: PillowSchema }])],
   providers: [PillowService],
   controllers: [PillowController]
 })
